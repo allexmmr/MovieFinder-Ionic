5a0048760ad8dee9d71187952405fcd6ade3c5d2
allexmmr <allexmmr@gmail.com>
2019-06-17 23:25:15 +1000

    Updated the API/json

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,7 +10,6 @@ export class HomePage {
   public popularMovies;
 
   constructor(private http: HttpClient, public navCtrl: NavController) {
-    // From: https://api.themoviedb.org/3/movie/popular?api_key=f843c14de4ce13c7041022ee34c428ec&language=en-US
-    this.popularMovies = this.http.get("https://api.myjson.com/bins/idzw4");
+    this.popularMovies = this.http.get("https://api.myjson.com/bins/wdwjh");
   }
 }
